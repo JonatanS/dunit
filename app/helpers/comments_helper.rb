e7bc1d83307f3a306b56f946596efb0507b58bb0
jonatans <jonatan@jschumacher.com>
2015-01-31 17:53:33 -0500

    comments save user id

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,8 +4,8 @@ module CommentsHelper
   	  text_field(:comment, :message) +
   	  submit_tag("Add Comment") +
   	  hidden_field(:comment, :subject_id, :value => element.id) +
-  	  hidden_field(:comment, :subject_type, :value => element.class.name)
+  	  hidden_field(:comment, :subject_type, :value => element.class.name) +
+  	  hidden_field(:comment, :user_id, :value => current_user.id)
   	end
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
  
   has_many :memberships
   has_many :projects, :through => :memberships
+  has_many :comments
 
   def name
   	return email if first_name.blank?
